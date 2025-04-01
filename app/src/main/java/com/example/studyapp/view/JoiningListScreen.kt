9bb49bce4f87c068016eafd22e437e6e93ce44ab
k6566 <rkqls4764?@naver.com>
2025-04-01 14:16:37 +0900

    [Feat] 가입한 스터디 목록 화면 추가

---
+++ app/src/main/java/com/example/studyapp/view/JoiningListScreen.kt
@@ -0,0 +1,52 @@
+package com.example.studyapp.view
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.studyapp.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun JoiningListScreen() {
+    val studyList = listOf("스터디1", "스터디2", "스터디3")
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("가입 중인 스터디") },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = colorResource(id = R.color.main_blue),
+                    titleContentColor = Color.White
+                )
+            )
+        }
+    ) { paddingValues ->
+        Column(modifier = Modifier.padding(paddingValues)) {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                items(studyList) { study ->
+                    StudyItem(study, "일정 관리", onButtonClick = {
+                        // 일정 관리 화면으로 이동
+                    })
+                }
+            }
+        }
+    }
+}
